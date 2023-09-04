4b5be97f1181148cf107d978c141d4eeb932e489
FleetAdmiralJakob <jakob.roessner@outlook.de>
2023-09-04 22:11:23 +0200

    Moved ConvexClientProvider.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "~/styles/globals.css";
 import { Inter } from "next/font/google";
 import { twMerge } from "tailwind-merge";
-import ConvexClientProvider from "~/app/ConvexClientProvider";
+import ConvexClientProvider from "~/ConvexClientProvider";
 
 const inter = Inter({
   subsets: ["latin"],
