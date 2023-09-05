35f6ee79874224b73fc9f06e6681707374ffae65
Federico <federico.leiva@mdevz.com>
2023-09-05 01:47:40 -0300

    add chat functionality and simple signin feature

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,132 @@
+"use client";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "~/ui/ui/form";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect, useRef, useState } from "react";
+import { Button } from "~/ui/button";
+import { useRouter } from "next/navigation";
+import { Textarea } from "~/ui/ui/textarea";
+
+const formSchema = z.object({
+  text: z.string().min(3),
+});
+
+export default function HomePage() {
+  const [loading, setLoading] = useState(true);
+
+  const router = useRouter();
+
+  const chats = useQuery(api.functions.chats.get, {});
+
+  const postChat = useMutation(api.functions.chats.post);
+
+  const chatsRef = useRef<HTMLDivElement>(null);
+
+  const [scrollToBottom, setScrollToBottom] = useState(true);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      text: "",
+    },
+  });
+
+  useEffect(() => {
+    setLoading(true);
+    const name = localStorage.getItem("name");
+    if (!name) {
+      return router.replace("/");
+    }
+    setLoading(false);
+
+    // I want to run this only once
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (scrollToBottom) {
+      chatsRef.current?.scrollTo({
+        top: chatsRef.current.scrollHeight,
+        behavior: "smooth",
+      });
+    }
+  }, [chats]);
+
+  const handleSubmit = async ({ text }: z.infer<typeof formSchema>) => {
+    const name = localStorage.getItem("name");
+    if (text && name) {
+      await postChat({
+        body: text,
+        user: name,
+      });
+      form.reset();
+    }
+  };
+
+  const handleScroll = () => {
+    if (!chatsRef.current) return;
+    const { scrollHeight, scrollTop, clientHeight } = chatsRef.current;
+    const scrollBottom = scrollHeight - scrollTop - clientHeight;
+    if (scrollBottom < 100 && !scrollToBottom) {
+      setScrollToBottom(true);
+    } else if (scrollBottom > 100 && scrollToBottom) {
+      setScrollToBottom(false);
+    }
+  };
+
+  return (
+    <main className="flex max-h-screen min-h-screen flex-col gap-8 p-24">
+      <div
+        ref={chatsRef}
+        onScroll={handleScroll}
+        className="flex flex-1 flex-col gap-2 overflow-y-scroll"
+      >
+        {chats?.map(({ _id, text, user }) => (
+          <div
+            key={_id.toString()}
+            className="flex flex-col gap-4 rounded-sm border p-4 shadow-md"
+          >
+            <span className="text-lg font-semibold">{user}</span>
+            <span>{text}</span>
+          </div>
+        ))}
+      </div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className="flex w-full gap-2"
+        >
+          <FormField
+            control={form.control}
+            name="text"
+            render={({ field }) => (
+              <FormItem className="flex-1">
+                <FormControl>
+                  <Textarea
+                    className="flex-1"
+                    placeholder="Your message..."
+                    disabled={loading}
+                    rows={3}
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button className="w-auto px-8" type="submit" disabled={loading}>
+            Send
+          </Button>
+        </form>
+      </Form>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,99 @@
 "use client";
-import { useQuery } from "convex/react";
+import { useMutation } from "convex/react";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Input } from "~/ui/ui/input";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/ui/ui/form";
+import { Button } from "~/ui/button";
 import { api } from "../../convex/_generated/api";
+import { useRouter } from "next/navigation";
+
+const formSchema = z.object({
+  name: z.string().min(3, {
+    message: "Name must be at least 3 characters long",
+  }),
+});
 
 export default function HomePage() {
-  const chats = useQuery(api.chats.get);
+  const router = useRouter();
+
+  const [loading, setLoading] = useState(true);
+
+  const userMutation = useMutation(api.functions.users.post);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const handleSubmit = async ({ name }: z.infer<typeof formSchema>) => {
+    try {
+      setLoading(true);
+      await userMutation({
+        name: name,
+      });
+      localStorage.setItem("name", name);
+      return router.push("/chat");
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    const name = localStorage.getItem("name");
+    if (name) {
+      router.push("/chat");
+    } else {
+      router.prefetch("/chat");
+      setLoading(false);
+    }
+
+    // I want to run this only once
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        {chats?.map(({ _id, text }: { _id: string, text: string}) => (
-            <div key={_id.toString()}>{text}</div>
-        ))}
-      </main>
-  )
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" disabled={loading} {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name. This name is not unique,
+                  anyone can use it.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" disabled={loading}>
+            Sign in
+          </Button>
+        </form>
+      </Form>
+    </main>
+  );
 }
